023e7fc130fb7930c2595c5264734194fa049628
andreamcm <cor16076@uvg.edu.gt>
2019-08-21 12:25:10 -0600

    Regresion lineal

--- laboratorio2.R
+++ laboratorio2.R
@@ -2,34 +2,84 @@
 # Ivette Cardona - 16020
 # Andrea María Cordón - 16076
 
+
+
+
 # Set del directorio donde estan los datos
+# -----------------------------------------
 # Andrea
 setwd("~/2019/UVG/Segundo Semestre/DataScience/Laboratorios/Laboratorio2/DSLaboratorio2")
 # Ivette
 setwd("C:/Users/DELL/Documents/UVG/VIII_Semestre/Data Science/Laboratorio1DataScience")
 
+
+
+# --------------------
 # Librerias a utilizar
+# --------------------
 library(class)
 library(caret)
 
 
+
+# --------------------
 # Lectura de los datos
 # --------------------
 datos <- read.csv("train.csv")
-datos_relevantes <- datos[, c("LotFrontage", "YearRemodAdd", "YearBuilt", "GarageCars", "PoolArea", "YrSold", "SalePrice")]
+datos_relevantes <- datos[, c("LotArea", "YearRemodAdd", "YearBuilt", "GarageCars", "PoolArea", "YrSold", "SalePrice")]
 
 
+
+# ----------------------------------------------
 # Separación de datos en entrenamiento y prueba
 # ----------------------------------------------
 set.seed(123)
 
-porciento <- 60/100 #Porciento en el que se partirÃ¡n los datos
-muestra<-sample(1:nrow(datos_relevantes),porciento*nrow(datos_relevantes))#Muestra aleatoria de numeros de un vector
+porciento <- 60/100 # 60% para training y 40% para pruebas
+muestra<-sample(1:nrow(datos_relevantes),porciento*nrow(datos_relevantes))# Seleccion de muestras aleatorias
 
-trainSet<-datos_relevantes[muestra,] #Obtengo las filas de los elementos que estan en el sector de muestra
-testSet<-datos_relevantes[-muestra,] #Obtengo las filas de los elementos que no estÃ¡n en el vector de muestra
+trainSet<-datos_relevantes[muestra,] # Grupo de entrenamiento
+testSet<-datos_relevantes[-muestra,] # Grupo de pruebas
+
 
 
 # Regresión lineal
 # ----------------
+modeloRL <- lm(SalePrice~., data = trainSet)
+summary(modeloRL)
+
+prediccionRL <- predict(modeloRL, newdata = testSet)
+testSet$rlprediccion <- prediccionRL
+prediccionRL
+
+diferencia <- abs(testSet$rlprediccion-testSet$SalePrice)
+diferencia
+
+cfmrl <- confusionMatrix(prediccionRL, testSet)
+
+
+#Generación del modelo
+modeloLinealSimple<-lm(mpg~wt, data = trainSet)
+summary(modeloLinealSimple)
+
+#predicción
+prediccion<-predict(modeloLinealSimple,newdata = testSet[,2:ncol(testSet)])
+# Se agrega la predicción al conjunto de entrenamiento
+testSet$mpgPred<-prediccion
+#Ver la diferencia entre lo real y lo predicho
+dif<-abs(testSet$mpgPred-testSet$mpg)
+
+#Hay que establecer la diferencia mínima para saber quienes son los mejores clasificados
+
+testSet$mpgPred <-NULL
+
+
+
+
+
+
+
+
+
+
 
